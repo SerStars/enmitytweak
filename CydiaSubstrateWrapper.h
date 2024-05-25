57ac2109464a2302de866009ecea081a5808bc0c
SerStars <90714930+SerStars@users.noreply.github.com>
2024-05-25 16:59:47 +0200

    Delete CydiaSubstrateWrapper.h

--- CydiaSubstrateWrapper.h
+++
@@ -1,9 +0,0 @@
-// CydiaSubstrateWrapper.h
-
-extern "C" {
-    // Include necessary C headers here
-}
-
-// Include C++ headers outside of the extern "C" block
-#include <mach-o/nlist.h>
-#include <CydiaSubstrate/CydiaSubstrate.h>
