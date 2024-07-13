cc0c84ad20893d14237bf03414e52dec474801af
SerStars <90714930+SerStars@users.noreply.github.com>
2024-07-13 15:14:31 +0200

    New Version

--- script.sh
+++ script.sh
@@ -2,4 +2,5 @@
 filename=$(curl -sL https://ipa.aspy.dev/discord/stable/ | grep ipa | tail -n 2 | cut -f 2 -d '>' | cut -f 1 -d '<')
 discord_ipa_url="https://ipa.aspy.dev/discord/stable/${filename:1}"
 echo "discord_ipa_url=${discord_ipa_url}" >> $GITHUB_ENV
-echo "DISCORD_VERSION=${filename:1}" >> $GITHUB_ENV
+discord_version="${filename%.ipa}"
+echo "DISCORD_VERSION=${discord_verison:1}" >> $GITHUB_ENV
