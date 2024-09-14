aae5b6f8409de3f7debdb67bd34461167e722a50
SerStars <90714930+SerStars@users.noreply.github.com>
2024-09-14 23:54:38 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-filename=$(curl -sL https://ipa.aspy.dev/discord/stable/ | grep ipa | tail -n 2 | cut -f 2 -d '>' | cut -f 1 -d '<')
-discord_ipa_url="https://ipa.aspy.dev/discord/stable/${filename:1}"
+filename=$(curl -sL https://ipa.aspy.dev/discord/testflight/ | grep ipa | tail -n 2 | cut -f 2 -d '>' | cut -f 1 -d '<')
+discord_ipa_url="https://ipa.aspy.dev/discord/testflight/${filename:1}"
 echo "discord_ipa_url=${discord_ipa_url}" >> $GITHUB_ENV
-echo "DISCORD_VERSION=${filename:1}" >> $GITHUB_ENV
+echo "DISCORD_VERSION=TF_${filename:1}" >> $GITHUB_ENV
